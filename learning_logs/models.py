3726ab266ae450e9a78056e80ea53dcaad254a26
lightbanes <m.osiak46@gmail.com>
2018-11-11 19:11:42 +0100

    first commit, models 'topic' and 'entry' added

---
+++ learning_logs/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Topic(models.Model):
+    """Temat poznawany przez użytkownika."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Zwraca reprezentację modelu w postaci ciągu tekstowego."""
+        return self.text
+
+
+class Entry(models.Model):
+    """Konkretne informacje o postępie w nauce"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Zwraca reprezentację modelu w postaci ciągu tekstowego."""
+        if len(self.text) > 50:
+            return self.text[:50] + "..."
+        else:
+            return self.text
