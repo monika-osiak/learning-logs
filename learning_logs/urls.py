639e88981f12b3df6750c6d1d0d026a87b197ce3
lightbanes <m.osiak46@gmail.com>
2018-11-12 00:13:53 +0100

    forms added (new topic, new entry, edit entry)

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,6 +1,5 @@
 """Definiuje wzorce adresów URL dla learning_logs."""
 
-from django.conf.urls import url
 from django.urls import re_path
 from . import views
 
@@ -13,4 +12,13 @@ urlpatterns = [
 
     # Strona szczegółowa dotycząca pojedynczego tematu
     re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+
+    # Strona przeznaczona do dodawania nowego tematu
+    re_path(r'^new_topic/$', views.new_topic, name='new_topic'),
+
+    # Strona przeznaczona do dodawania nowego wpisu
+    re_path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+
+    # Strona przeznaczona do edycji wpisu
+    re_path(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]
