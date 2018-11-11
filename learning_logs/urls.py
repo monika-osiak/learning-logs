94640827ab13197d4d5c2aedf88b8203eeb62a68
lightbanes <m.osiak46@gmail.com>
2018-11-11 21:11:30 +0100

    added views for all topics and single topic detailed

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,9 +1,16 @@
 """Definiuje wzorce adresów URL dla learning_logs."""
 
 from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
     # Strona główna
-    url(r'^$', views.index, name='index'),
+    re_path(r'^$', views.index, name='index'),
+
+    # Wyświetlanie wszystkich tematów
+    re_path(r'^topics/$', views.topics, name='topics'),
+
+    # Strona szczegółowa dotycząca pojedynczego tematu
+    re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """Strona główna dla aplikacji Learning Log."""
     return render(request, '../templates/learning_logs/index.html')
+
+def topics(request):
+    """Wyświetlenie wszystkich tematów."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, '../templates/learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Wyświetla pojedynczy temat i wszystkie powiązane z nim wpisy."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, '../templates/learning_logs/topic.html', context)
